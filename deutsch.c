a9c57cf000977f80261eccda1e583ee3dc352843
SEGFAULT <IGBC@users.noreply.github.com>
2016-09-18 00:29:18 +0200

    Die Beispiel-Hauptfunktion für Deutsches C-hochgeladen.

---
+++ deutsch.c
@@ -0,0 +1,8 @@
+#include "deutsch.h"
+#define text "deise ding war ein doof idee; aber es hat gefunctioniert"
+ganzzahl main(zeichen *paramenteliste, ganzzahl *paramentezahl) {
+    fuer(ganzzahl i = 0; i<10; i++) {
+        printf("Hallo Welt %i\n", i);
+    }
+    zurueck nichts; 
+}
