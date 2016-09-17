762b1b432b7c1795de45acf42bb1fd0b1fe88d18
SEGFAULT <IGBC@users.noreply.github.com>
2016-09-18 00:18:36 +0200

    die Kopfdatei für Deutsches C hochgeladen.
    Erste

---
+++ deutsch.h
@@ -0,0 +1,62 @@
+//schlüsselworte
+#define selbst auto
+#define doppelgleit double
+#define ganzzahl int
+#define verbund struct
+#define raus break
+#define sonst else
+#define langganzzahl long
+#define unterscheide switch
+#define fall case
+#define aufzaehlung enum
+#define schiebe register
+#define definieretyp typedef
+#define zeichen char
+#define ausserhalb extern
+#define zurueck return
+#define vereint union
+#define konstant const
+#define gleitkommazahl float
+#define kleinganzzahl short
+#define vorzeichenfrei unsigned
+#define setzefort continue
+#define fuer for
+#define vorzeichenhaft signed
+#define undefinert void
+#define standard default
+#define gehezu goto
+#define groesse sizeof
+#define fluechtig volatile
+#define mache do
+#define wenn if
+#define statisch static
+#define solange while
+
+//konstanten
+#define wahr 1
+#define falsch 0
+#define nichts 0
+
+//main
+#define hauptfunktion main
+
+//posix
+#define besetze malloc
+#define befreie free
+#define leerbesetze calloc
+#define wiederbesetze realloc
+
+//standardbibliothek
+#define druckformatiert printf
+#define dateidruckformatiert fprintf
+#define zeichenkettedruckformatiert sprintf
+
+#define beteuere assert
+
+//präprozessoranweisungen
+//nichts funktionert
+/*#define #definiere #define
+#define #importiere #include 
+#define #istdef #ifdef
+#define #istndef #ifndef
+#define #endewenn #endif*/
